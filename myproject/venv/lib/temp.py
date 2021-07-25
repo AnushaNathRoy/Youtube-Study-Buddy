ce1dd31660337e94d0ea4a20595aa51a6c9b6b71
AnushaNathRoy <scribble1001@gmail.com>
2021-07-25 18:32:01 +0400

    web app

--- myproject/venv/lib/temp.py
+++ myproject/venv/lib/temp.py
@@ -6,6 +6,7 @@ import csv
 from urllib.parse import urlparse
 from fuzzywuzzy import fuzz
 import urllib.parse
+import datetime
 import pandas as pd
 
 app = Flask(__name__)
@@ -26,6 +27,10 @@ def response():
 			return "Please Enter a Valid Youtube URL"
 		youtube_id = query_data['v'][0]
 
+		search_query = request.form.get("search")
+		search_words = []
+		search_words = search_query.split(" ")
+		
 		transcript = YouTubeTranscriptApi.get_transcript(youtube_id)
 		transcript_search_data=[]
 		transcript_text=""
@@ -33,8 +38,28 @@ def response():
 		for line in transcript:
 			transcript_line+=line['text'].replace("\n"," ")
 			transcript_text+=transcript_line
+			line["start"] = str(datetime.timedelta(seconds=int(line["start"])))
+			rank = fuzz.partial_ratio(search_query.lower(),transcript_line.lower())
+			for word in search_words:
+				if (transcript_line.find(word) != -1):
+					rank+=50
+			line['rank'] = rank
 
-		return render_template("index.html",transcript_content=transcript_text)
+		with open ('transcript_csv.csv','w') as w:
+			fieldnames = transcript[0].keys()
+			wwrite = csv.DictWriter(w,fieldnames=fieldnames)
+			wwrite.writeheader()
+			wwrite.writerows(transcript)
+
+		csv_data = pd.read_csv("transcript_csv.csv")
+		sorted_csv_data = csv_data.sort_values(by=["rank"], ascending=False)
+		sorted_csv_data.to_csv("transcript_sorted.csv",index=False)
+
+		w.close()
+
+		r = open("transcript_sorted.csv","r")
+		data=list(csv.DictReader(r))
+		return render_template("index.html",transcript_content=transcript_text,video_urlp=youtube_id,transcript_data=transcript,transcript_datas = data)
 
 if __name__ == '__main__':
 	app.run()
